37fde3b262244e264b7872b0336a379b63c90659
OVI <victorogugua44@gmail.com>
2022-06-27 20:58:41 +0100

    fix: bug-fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,40 +19,30 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        // User::create([
-        //     'name' => 'Admin',
-        //     'email' => 'admin@admin.com',
-        //     'password' => Hash::make('password')
-        // ]);
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password')
+        ]);
 
-        // $company = Company::factory()->create([
-        //     'name' => 'Test Test',
-        //     'email' => 'test@test.com', 
-        //     'website' => 'https://test.com'
-        // ]);
+        $company = Company::factory()->create([
+            'name' => 'Test Test',
+            'email' => 'test@test.com', 
+            'website' => 'https://test.com'
+        ]);
 
-        // $company2 = Company::factory()->create([
-        //     'name' => 'Company 2',
-        //     'email' => 'test@test.com', 
-        //     'website' => 'https://test.com'
-        // ]);
+        $company2 = Company::factory()->create([
+            'name' => 'Company 2',
+            'email' => 'test@test.com', 
+            'website' => 'https://test.com'
+        ]);
 
-        // $company3 = Company::factory()->create([
-        //     'name' => 'Company 3',
-        //     'email' => 'test@test.com', 
-        //     'website' => 'https://test.com'
-        // ]);
+        Employee::factory(10)->create([
+            'company_id' => $company->id
+        ]);
 
-        // Employee::factory(10)->create([
-        //     'company_id' => $company->id
-        // ]);
-
-        // Employee::factory(10)->create([
-        //     'company_id' => $company2->id
-        // ]);
-        
-        // Employee::factory(10)->create([
-        //     'company_id' => $company3->id
-        // ]);
+        Employee::factory(10)->create([
+            'company_id' => $company2->id
+        ]);
     }
 }
